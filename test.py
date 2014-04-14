02f1d5bdd6a2af796284502e38890cc8fe85b2fe
Ben Pringle <ben.pringle@gmail.com>
2014-04-14 00:34:18 -0400

    Add test for subfunction within corolet

--- test.py
+++ test.py
@@ -12,12 +12,18 @@ def myclet(num):
     print('[myclet] start with num =', num)
     result = corolet.yield_from(subcoro(num))
     print('[myclet] result =', result)
+    subfunc_sleep(.5)
     return result * 2
 
 @asyncio.coroutine
 def subcoro(num):
     print('[subcoro] start with num =', num)
     return num * 2
+
+def subfunc_sleep(time):
+    print('[subfunc_sleep] sleeping for', time)
+    corolet.yield_from(asyncio.sleep(time))
+    print('[subfunc_sleep] done sleeping')
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
